ca5efbe53197e3690a1df4b4b37c1a019f1d0f73
Casey <james0678@ajou.ac.kr>
2025-02-15 10:41:10 +0900

    한 번 더 수정..

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { React } from "react";
+import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="31578713827-k1rbd28tbuv0imu7fk16pilfg9lgii42.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
-  </StrictMode>
+  <App />
 );
