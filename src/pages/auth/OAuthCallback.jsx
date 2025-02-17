c97350828ceaf127b019af034278de3903bba120
Casey <james0678@ajou.ac.kr>
2025-02-17 20:11:04 +0900

    진짜 되어야 함

--- src/pages/auth/OAuthCallback.jsx
+++ src/pages/auth/OAuthCallback.jsx
@@ -8,14 +8,16 @@ function OAuthCallback() {
   useEffect(() => {
     console.log('[Callback] 콜백 페이지 진입')
     console.log('[Callback] 현재 URL:', window.location.href)
+    console.log('[Callback] pathname:', location.pathname)
     console.log('[Callback] URL 파라미터:', location.search)
+    console.log('[Callback] 전체 location 객체:', location)
     
     const searchParams = new URLSearchParams(location.search)
-    const isNewUser = searchParams.get('isNewUser') === 'true'
-    console.log('[Callback] isNewUser 파라미터:', searchParams.get('isNewUser'))
-    console.log('[Callback] isNewUser 변환값:', isNewUser)
+    const isNewUser = searchParams.get('isNewUser')
+    console.log('[Callback] isNewUser 원본값:', isNewUser)
+    console.log('[Callback] isNewUser 변환값:', isNewUser === 'true')
 
-    if (isNewUser) {
+    if (isNewUser === 'true') {
       console.log('[Callback] 새 사용자 감지 -> /auth/add로 이동')
       navigate('/auth/add')
     } else {
