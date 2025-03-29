888630a8afb344489143a434807559d87240578f
Casey <james0678@ajou.ac.kr>
2025-03-29 19:16:03 +0900

    api.js 재수정

--- src/services/api.js
+++ src/services/api.js
@@ -1386,7 +1386,7 @@ export const studyService = {
 
       // API 요청
       const response = await api.post(
-        `/studies/${studyId}management/members/add`,
+        `/studies/${studyId}/management/members/add`,
         memberData,
         {
           headers: {
