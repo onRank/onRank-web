a880371a52ca1008ed5a4ffb5b2d27077453bad5
Kwak Minseo <mkms0222@ajou.ac.kr>
2025-05-08 15:45:00 +0900

    fix: 스터디 페이지 css 수정

--- src/components/study/tabs/DefaultContent.jsx
+++ src/components/study/tabs/DefaultContent.jsx
@@ -101,33 +101,31 @@ function DefaultContent({ studyData }) {
   return (
     <div style={styles.wrapper}>
       <div style={styles.main}>
-        <div style={styles.content}>
-          <div style={styles.grid}>
-            {/* ① PointContainer */}
-            <div style={styles.topLeft}>
-              {points.map((p, i) => (
-                <PointContainer
-                  key={i}
-                  title={p.title}
-                  icon={p.icon}
-                  point={p.point}
-                />
-              ))}
-            </div>
-
-            {/* ② MyRank */}
-            <div style={styles.topRight}>
-              <MyRank {...myRank} />
-            </div>
-
-            {/* ③ Ranking */}
-            <div style={styles.bottomLeft}>
-              <Ranking rankingData={rankingData} />
-            </div>
-
-            {/* ④ RankingList */}
-            <RankingList rankingList={rankingList} />
+        <div style={styles.grid}>
+          {/* ① PointContainer */}
+          <div style={styles.topLeft}>
+            {points.map((p, i) => (
+              <PointContainer
+                key={i}
+                title={p.title}
+                icon={p.icon}
+                point={p.point}
+              />
+            ))}
           </div>
+
+          {/* ② MyRank */}
+          <div style={styles.topRight}>
+            <MyRank {...myRank} />
+          </div>
+
+          {/* ③ Ranking */}
+          <div style={styles.bottomLeft}>
+            <Ranking rankingData={rankingData} />
+          </div>
+
+          {/* ④ RankingList */}
+          <RankingList rankingList={rankingList} />
         </div>
       </div>
     </div>
